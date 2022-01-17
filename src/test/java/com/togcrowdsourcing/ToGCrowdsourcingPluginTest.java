954076f7c81dce0891b52cb516fd963fa219a75d
jcarbelbide <jcarbelbide@gmail.com>
2022-01-16 20:59:35 -0800

    Changed plugin name from ToGWorldCrowdsourcing to ToGCrowdsourcing. Forgot test files, so adding them now.

--- src/test/java/com/togcrowdsourcing/ToGCrowdsourcingPluginTest.java
+++ src/test/java/com/togcrowdsourcing/ToGCrowdsourcingPluginTest.java
@@ -1,10 +1,10 @@
-package net.runelite.client.plugins.togcrowdsourcing.src.test.java.com.togworldcrowdsourcing;
+package net.runelite.client.plugins.togcrowdsourcing.src.test.java.com.togcrowdsourcing;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 import net.runelite.client.plugins.togcrowdsourcing.src.main.java.com.togcrowdsourcing.ToGCrowdsourcingPlugin;
 
-public class ToGWorldCrowdsourcingPluginTest
+public class ToGCrowdsourcingPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
